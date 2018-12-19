f3013f7730ad0f3a899e8636e8cec1e0fbcbe6a2
TomMousset <wellandylan@gmail.com>
2018-12-19 12:32:06 +0100

    application Conversion degrés

--- src/sdz1.java
+++ src/sdz1.java
@@ -3,39 +3,42 @@ import java.util.Scanner;
 public class sdz1 {
 
 	public static void main(String[] args){
+		int choix1;
+		float resultat;
+		char choix2;
 		
-		System.out.println("\n\n--------------------------------------Conversation--------------------------------------\n\n");
-		System.out.println("Ordi :\t Veuillez saisir votre prénom !");
-		System.out.print("Moi :\t");
-		Scanner sc1 = new Scanner(System.in);
-		String prenom = sc1.nextLine();
-		System.out.println("Ordi :\tOK !");
-		System.out.println("Ordi :\tTiens voila Jaques qui arrive !");
-		System.out.println("Jaques :\tBonjour !");
+		System.out.println("-------------------------------------------------\n");
+		System.out.println("CONVERTISSEUR DEGRÉS CELSIUS ET DEGRÉS FAHRENHEIT\n");
+		do {
+			System.out.println("-------------------------------------------------\n");
 		
-		System.out.print("Moi :\t");
-		Scanner sc2 = new Scanner(System.in);
-		String mot = sc2.nextLine();
-
-		switch(mot) {
-		  case "Bonjour":
-		    System.out.println("Jaques :\tFraîche matinée pas vrai !");
-		    break;
-		  case "Bonsoir":
-		    System.out.println("Jaques :\tMais on n'est pas le soir !");
-		    break;
-		  case "Salut":
-		    System.out.println("Jaques :\tEt la politesse, tu as oublié ???");
-		    break;
-		  case "":
-		    System.out.println("Jaques :\tAlors tu me réponds ???");
-		    break;
-		  default:
-		    System.out.println("Jaques :\tDe quoi ???");
-		}
+			do {
+				System.out.println("choisissez le mode de convertion :");
+				System.out.println("1 - Convertisseur Celsius -> Fahrenheit");
+				System.out.println("2 - Convertisseur Fahrenheit -> Celsius");
 		
-		System.out.println("\n\n------------------------------------------Fin-------------------------------------------\n\n");
-																	  
+				Scanner sc1 = new Scanner(System.in);
+				choix1 = sc1.nextInt();
+			}while(choix1 != 1 && choix1 != 2);
+		
+			System.out.println("température à convertir :");
+			Scanner sc2 = new Scanner(System.in);
+			float valeur = sc2.nextInt();
+		
+			if(choix1 == 1) {
+				resultat = 9f / 5f * valeur + 32;
+				System.out.println(valeur +" °C correspondent à " + resultat + "°F");
+			}else {
+				resultat = ((valeur - 32) * 5) / 9;
+				System.out.println(valeur +" °F correspondent à " + resultat + "°C");
+			}
+			do {
+				System.out.println("Souhaitez-vous convertir une autre température?(O/N)");
+				Scanner sc3 = new Scanner(System.in);
+				choix2 = sc3.nextLine().charAt(0);
+			}while(choix2 != 'O' && choix2 != 'N');
+		} while(choix2 == 'O');
+		System.out.println("Au revoir");
 	}
 }
 
