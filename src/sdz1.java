69d2157a33c88910fcec94acc02a4ce55a12a9d4
TomMousset <wellandylan@gmail.com>
2018-12-20 09:42:25 +0100

    application température terminée

--- src/sdz1.java
+++ src/sdz1.java
@@ -2,10 +2,16 @@ import java.util.Scanner;
 
 public class sdz1 {
 
+	private static Scanner sc3;
+	private static Scanner sc2;
+	private static Scanner sc1;
+
 	public static void main(String[] args){
-		int choix1;
+		String choix1;
+		float reponse = new Float(0);
 		float resultat;
-		char choix2;
+		String choix2;
+		boolean conformite;
 		
 		System.out.println("-------------------------------------------------\n");
 		System.out.println("CONVERTISSEUR DEGRÉS CELSIUS ET DEGRÉS FAHRENHEIT\n");
@@ -17,28 +23,42 @@ public class sdz1 {
 				System.out.println("1 - Convertisseur Celsius -> Fahrenheit");
 				System.out.println("2 - Convertisseur Fahrenheit -> Celsius");
 		
-				Scanner sc1 = new Scanner(System.in);
-				choix1 = sc1.nextInt();
-			}while(choix1 != 1 && choix1 != 2);
+				sc1 = new Scanner(System.in);
+				choix1 = sc1.next();
+			}while(!choix1.equals("1") && !choix1.equals("2"));
 		
+			do {
 			System.out.println("température à convertir :");
-			Scanner sc2 = new Scanner(System.in);
-			float valeur = sc2.nextInt();
-		
-			if(choix1 == 1) {
-				resultat = 9f / 5f * valeur + 32;
-				System.out.println(valeur +" °C correspondent à " + resultat + "°F");
+			sc2 = new Scanner(System.in);
+			String valeur = sc2.next();	
+
+			try {
+			    reponse = Float.valueOf(valeur);
+			    conformite = true;
+			}
+			catch (NumberFormatException nfe) {
+				conformite = false;
+			}
+			}while(!conformite);
+			
+			if(choix1.equals("1")) {
+				resultat = 9f / 5f * reponse + 32;
+				System.out.println(reponse +" °C correspondent à " + resultat + "°F");
 			}else {
-				resultat = ((valeur - 32) * 5) / 9;
-				System.out.println(valeur +" °F correspondent à " + resultat + "°C");
+				resultat = ((reponse - 32) * 5) / 9;
+				System.out.println(reponse +" °F correspondent à " + resultat + "°C");
 			}
 			do {
 				System.out.println("Souhaitez-vous convertir une autre température?(O/N)");
-				Scanner sc3 = new Scanner(System.in);
-				choix2 = sc3.nextLine().charAt(0);
-			}while(choix2 != 'O' && choix2 != 'N');
-		} while(choix2 == 'O');
+				sc3 = new Scanner(System.in);
+				choix2 = sc3.nextLine();
+				choix2 = choix2.toUpperCase();
+				System.out.println(choix2);
+				if(choix2.isEmpty()){
+					choix2 = "O";
+				}
+			}while(!choix2.equals("O") && !choix2.equals("N"));
+		} while(choix2.equals("O"));
 		System.out.println("Au revoir");
 	}
 }
-
